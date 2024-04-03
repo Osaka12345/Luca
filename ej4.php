9232879fdd77a0e37acfb2f17d66ba88d0883657
Luca Cristaldo <lucacristldo@gmail.com>
2024-04-03 09:12:01 -0300

    Tarea 4

---
+++ ej4.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $nombre = 'Manolo ASDA';
+        $edad = 18; //enteros (Integer)
+        $altura =1.72;
+        $mayoredad = true;
+       
+        /* 
+            1- Falta el punto y coma
+            2- El nombre de la variable puede confundir
+            3- Puede confundir ya que el nombre 
+               de la variable es numero pero almacena un dato de tipo Stirng
+            4- No declara al variable y el nombre comienza con mayus
+            5- No declara la variable y no hay ";"
+            6- El "$" no deberia estar
+
+            Luca Cristaldo
+            Felipe Cuiñas
+        */
+    ?>
+</body>
+</html>
