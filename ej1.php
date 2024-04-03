443e7c215057fb7ffd4d9f8a00d344472cb4bde5
Luca Cristaldo <lucacristldo@gmail.com>
2024-04-03 08:29:45 -0300

    ej1

---
+++ ej1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Letra</h1>
+    <?php
+        echo 'No se si hice bien, pero llame';
+        echo ' No me gusto lo que escuche';
+        echo ' y ella me llama y me siento solo';
+        echo ' el miedo me come y no entiendo comoo';
+    
+    ?>
+    <img src="momotym.jpg" alt="">
+
+</body>
+</html>
