55822e7f226aa417b0d6429b323dde6b31465f83
Luca Cristaldo <lucacristldo@gmail.com>
2024-04-03 08:27:24 -0300

    ej2

---
+++ ej2.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1> Mi segundo archivo php</h1>
+    <?php
+    echo <<<END
+    En un mundo con esperanza 
+    en donde me llama y me llama
+    END; ?>
+</body>
+</html>
