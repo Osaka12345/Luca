372c06566139b3067058c1478127bd4b73fdd065
Luca Cristaldo <lucacristldo@gmail.com>
2024-04-03 08:44:15 -0300

    hola world

---
+++ ej3.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    // Esto es un comentario de una sola linea
+    echo 'esto es una prueba';
+    /*Esto es un comentsrio multilinea
+    y otra linea de comentarios */
+    echo 'esto es una preuaba';
+    echo 'srgbhu';
+    #Esto es un comentario al estilo de cojsnodfjsopla de una sola lineeaa
+    ?>
+</body>
+</html>
